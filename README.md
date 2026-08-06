a9794113c3c52e8884ce941fb5eef4596b20d856
pardeep1916P <chaitanyacharan07@gmail.com>
2026-08-07 01:58:24 +0530

    feat(leetcode): add solution for Merge Two Sorted Lists [Java]

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 | # | Problem | Platform | Language |
 | :--- | :--- | :--- | :--- |
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum) | LeetCode #1 | [Java](./two-sum/two-sum.java) |
+| 2 | [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists) | LeetCode #21 | [Java](./merge-two-sorted-lists/merge-two-sorted-lists.java) |
 
 ## Tree
 | # | Problem | Platform | Language |
 | :--- | :--- | :--- | :--- |
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum) | LeetCode #1 | [Java](./two-sum/two-sum.java) |
+| 2 | [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists) | LeetCode #21 | [Java](./merge-two-sorted-lists/merge-two-sorted-lists.java) |
 
 ---
 *Synced automatically using [CodeSync](https://github.com/pardeep1916P/codeSync).*
---
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) { //if llist1 first node is smallest
+                curr.next = list1;
+                list1 = list1.next;
+            } else { //if llist2 first node is smallest
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if (list1 != null) curr.next = list1; //if llist2 is empty
+        if (list2 != null) curr.next = list2; //if llist1 is empty
+        return dummy.next;
+    }
+}
